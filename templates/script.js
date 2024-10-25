3b594bc6c72165d7e703eb791d83a1319f00ec11
Mark Andrew Soliman <markandrewsoliman@outlook.com>
2024-10-25 22:18:28 +0800

    seller dashboard pending

--- templates/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('scroll-right').addEventListener('click', function() {
-    const container = document.getElementById('collections-container');
-    container.scrollBy({
-        left: 330,
-        behavior: 'smooth'
-    });
-});
